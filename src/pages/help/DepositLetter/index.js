d530123a9217fc3fd8ab4d72a70b9967e3fd1dc6
Konstantin Brazhnik <konst@ntin.solutions>
2020-06-17 12:12:51 -0500

    Page Placeholders

---
+++ src/pages/help/DepositLetter/index.js
@@ -0,0 +1,35 @@
+import React, {Fragment} from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+import {Container, Row, Col, Button} from 'reactstrap';
+
+import PageHeader from '../../../components/PageHeader/PageHeader';
+
+function Page() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Imagine a better legal system.`}
+      description="We're building a new legal system and we're thinking like programmers. Help us or get help.<head />">
+      <PageHeader>
+        <h2>Deposit Letter</h2>
+      </PageHeader>
+      <main>
+        <section>
+          <Container>
+            <Row>
+              <h5>Page Content</h5>
+            </Row>
+          </Container>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export default Page;
